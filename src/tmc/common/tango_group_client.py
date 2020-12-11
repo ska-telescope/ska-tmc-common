80871b29dde3ca257ad7f2abff3b36da17019633
shraddhabajare <shraddhab.ska@gmail.com>
2020-12-11 17:01:29 +0530

    AT1-706:Implement group command api methods inside TangoGroupClient class.

--- src/tmc/common/tango_group_client.py
+++ src/tmc/common/tango_group_client.py
@@ -17,6 +17,8 @@ from tango.server import run,attribute, command, device_property
 
 import logging
 
+LOGGER = logging.getLogger(__name__)
+
 class TangoGroupClient:
     """
     
@@ -32,27 +34,81 @@ class TangoGroupClient:
         return tango.Group(group_name)
 
     def add_device(self, devices_to_add):
-        for device in devices_to_add:
-            self.tango_group.add(device)
+        try:
+            for device in devices_to_add:
+                self.tango_group.add(device)
+        except DevFailed as dev_failed:
+            self.logger.exception("Failed to add device")
+            tango.Except.re_throw_exception(dev_failed,
+                "Failed to add device",
+                str(dev_failed),
+                "TangoGroupClient.add_device()",
+                tango.ErrSeverity.ERR)  
 
     def remove_device(self, devices_to_remove):
-        for device in devices_to_add:   
-            self.tango_group.remove(device)
+        try:
+            for device in devices_to_remove:
+                self.tango_group.remove(device)
+        except DevFailed as dev_failed:
+            self.logger.exception("Failed to remove device")
+            tango.Except.re_throw_exception(dev_failed,
+                "Failed to remove device",
+                str(dev_failed),
+                "TangoGroupClient.remove_device()",
+                tango.ErrSeverity.ERR)
 
-    def delete_device(self, devices_to_delete)
-        for device in devices_to_add:
+    def delete_group(self, group_to_delete):
+        try:
             self.tango_group.delete(device)
+        except DevFailed as dev_failed:
+            self.logger.exception("Failed to delete group")
+            tango.Except.re_throw_exception(dev_failed,
+                "Failed to remove device",
+                str(dev_failed),
+                "TangoGroupClient.delete_group()",
+                tango.ErrSeverity.ERR)
+
+    def get_device_list(self, forward=True):
+        """
+            :param argin: bool
+            :return: The list of devices
+
+            :rtype: str
+        """
+        try:
+            self.device_list
+        except DevFailed as dev_failed:
+            self.logger.exception("Failed to get device list")
+            tango.Except.re_throw_exception(dev_failed,
+                "Failed to get device list",
+                str(dev_failed),
+                "TangoGroupClient.get_device_list()",
+                tango.ErrSeverity.ERR)  
+
+    def remove_all(self):
+        return self.tango_group.remove_all()
 
     def send_command(self, command_name, input_arg = None):
         try:
             self.tango_group.command_inout(command_name, input_arg)
         except DevFailed as dev_failed:
             self.logger.exception("Failed to execute command .")
-            tango.Except.throw_exception(dev_failed,
+            tango.Except.re_throw_exception(dev_failed,
                 "Failed to execute command .",
                 str(dev_failed),
                 "TangoGroupClient.send_command()",
-                tango.ErrSeverity.ERR)    
+                tango.ErrSeverity.ERR)  
+
+    def send_command_async(self, command_name, input_arg = None):
+        try:
+            self.tango_group.command_inout_asynch(command_name, input_arg)
+        except DevFailed as dev_failed:
+            self.logger.exception("Failed to execute command .")
+            tango.Except.re_throw_exception(dev_failed,
+                "Failed to execute command .",
+                str(dev_failed),
+                "TangoGroupClient.send_command_async()",
+                tango.ErrSeverity.ERR)      
 
 def main(args=None, **kwargs):
     """
