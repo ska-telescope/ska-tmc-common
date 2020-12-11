0706a10cc71aafed3462028d732695a5fbb438f4
shraddhabajare <shraddhab.ska@gmail.com>
2020-12-11 15:23:54 +0530

    AT1-706:Implement TangoGroupClient class

--- src/tmc/common/tango_group_client.py
+++ src/tmc/common/tango_group_client.py
@@ -39,11 +39,21 @@ class TangoGroupClient:
         for device in devices_to_add:   
             self.tango_group.remove(device)
 
+    def delete_device(self, devices_to_delete)
+        for device in devices_to_add:
+            self.tango_group.delete(device)
+
     def send_command(self, command_name, input_arg = None):
         try:
             self.tango_group.command_inout(command_name, input_arg)
         except DevFailed as dev_failed:
-            print("DevFailed exception is:", dev_failed)
+            self.logger.exception("Failed to execute command .")
+            tango.Except.throw_exception(dev_failed,
+                "Failed to execute command .",
+                str(dev_failed),
+                "TangoGroupClient.send_command()",
+                tango.ErrSeverity.ERR)    
+
 def main(args=None, **kwargs):
     """
     Main function of the TangoGroupClient module.
