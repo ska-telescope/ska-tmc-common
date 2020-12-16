04eb60d62515b94c94cd59572a128943686550a5
shraddhabajare <shraddhab.ska@gmail.com>
2020-12-16 14:16:48 +0530

    AT1-706:Add tango_context.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,42 @@
 """
 A module defining a list of fixtures that are shared across all ska.base tests.
 """
+"""
+A module defining a list of fixture functions that are shared across all the skabase
+tests.
+"""
+import importlib
+import mock
+import pytest
+from tango import DeviceProxy
+from tango.test_context import DeviceTestContext
+
+
+@pytest.fixture(scope="class")
+def tango_context(request):
+    """Creates and returns a TANGO DeviceTestContext object.
+
+    Parameters
+    ----------
+    request: _pytest.fixtures.SubRequest
+        A request object gives access to the requesting test context.
+    """
+    # TODO: package_name and class_name can be used in future
+    # fq_test_class_name = request.cls.__module__
+    # fq_test_class_name_details = fq_test_class_name.split(".")
+    # package_name = fq_test_class_name_details[1]
+    # class_name = module_name = fq_test_class_name_details[1]
+    # module = importlib.import_module("{}.{}".format(package_name, module_name))
+    # klass = getattr(module, class_name)
+    properties = {'CspSubarrayLNFQDN': 'ska_mid/tm_leaf_node/csp_subarray01',
+                  'SdpSubarrayLNFQDN': 'ska_mid/tm_leaf_node/sdp_subarray01',
+                  'DishLeafNodePrefix': 'ska_mid/tm_leaf_node/d',
+                  'SdpSubarrayFQDN': 'mid_sdp/elt/subarray_1',
+                  'CspSubarrayFQDN': 'mid_csp/elt/subarray_01'}
+    module = importlib.import_module("src/tmc/common")
+    klass = getattr(module, "TangoClient")
+    tango_context = DeviceTestContext(klass, properties=properties)
+    tango_context.start()
+    klass.get_name = mock.Mock(side_effect=tango_context.get_device_access)
+    yield tango_context
+    tango_context.stop()
