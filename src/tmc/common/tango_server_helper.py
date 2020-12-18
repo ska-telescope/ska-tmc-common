e501438f9f704544347d8ed1334ff4c4366d6dcf
shraddhabajare <shraddhab.ska@gmail.com>
2020-12-18 15:39:19 +0530

    AT1-706:Resolve review comments

--- src/tmc/common/tango_server_helper.py
+++ src/tmc/common/tango_server_helper.py
@@ -33,8 +33,8 @@ class TangoServerHelper:
         # Maintain a map
         # key of map is the string containing property name
         # value of map is the tango.property class object (defined in Tango class)
-        #self.prop_map = dict("DishLeafNodePrefix", self.device.DishLeafNodePrefix)
-        self.prop_map = None
+        self.prop_map = dict("DishLeafNodePrefix", self.device.DishLeafNodePrefix)
+        #self.prop_map = None
 
     @staticmethod
     def get_instance():
