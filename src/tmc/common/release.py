6b751ce14d20803dfb07e23b483d47c1e7e8d8e4
Apurva Patkar <apurva.ska@gmail.com>
2021-04-06 05:18:41 +0000

    AT1-824 Publish new release of ska-tmc-common 0.1.6

--- src/tmc/common/release.py
+++ src/tmc/common/release.py
@@ -7,7 +7,7 @@
 """Release information for ska-tmc-common Python Package"""
 
 name = """skatmccommon"""
-version = "0.1.5"
+version = "0.1.6"
 version_info = version.split(".")
 description = """A set of common classes for TMC devices."""
 author = "SKA India NCRA"
