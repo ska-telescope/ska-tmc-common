7a03ac2f56fad1586732cf7c1064be3e9acda610
Aditya Dange <adityadange.ska@gmail.com>
2021-12-28 17:27:44 +0530

    AT1-1028 Added test cases for adapter class.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
 import importlib
+import logging
 
 import mock
 import pytest
-from tango.test_context import DeviceTestContext
+from tango.test_context import DeviceTestContext, MultiDeviceTestContext
+
+from ska_tmc_common.dev_factory import DevFactory
 
 """
 A module defining a list of fixtures that are shared across all ska.base tests.
@@ -25,3 +28,35 @@ def tango_context(request):
     klass.get_name = mock.Mock(side_effect=tango_context.get_device_access)
     yield tango_context
     tango_context.stop()
+
+
+def pytest_addoption(parser):
+    """
+    Pytest hook; implemented to add the `--true-context` option, used to
+    indicate that a true Tango subsystem is available, so there is no
+    need for a :py:class:`tango.test_context.MultiDeviceTestContext`.
+    :param parser: the command line options parser
+    :type parser: :py:class:`argparse.ArgumentParser`
+    """
+    parser.addoption(
+        "--true-context",
+        action="store_true",
+        default=False,
+        help=(
+            "Tell pytest that you have a true Tango context and don't "
+            "need to spin up a Tango test context"
+        ),
+    )
+
+
+@pytest.fixture(scope="module")
+def tango_context_multitest(devices_to_load, request):
+    true_context = request.config.getoption("--true-context")
+    logging.info("true context: %s", true_context)
+    if not true_context:
+        with MultiDeviceTestContext(devices_to_load, process=False) as context:
+            DevFactory._test_context = context
+            logging.info("test context set")
+            yield context
+    else:
+        yield None
---
+++ tests/test_adapters.py
@@ -0,0 +1,36 @@
+import pytest
+from ska_tango_base.base.base_device import SKABaseDevice
+from ska_tango_base.subarray import SKASubarray
+from ska_tmc_common.adapters import BaseAdapter, SubArrayAdapter
+from ska_tmc_common.adapters import AdapterFactory, AdapterType
+
+
+@pytest.fixture(scope="module")
+def devices_to_load():
+    return (
+        {
+            "class": SKASubarray,
+            "devices": [{"name": "test/device/1"}],
+        },
+        {
+            "class": SKABaseDevice,
+            "devices": [
+                {"name": "test/device/2"},
+            ],
+        },
+    )
+
+
+def test_get_or_create_base_adapter(tango_context_multitest):
+    factory = AdapterFactory()
+    base_adapter = factory.get_or_create_adapter(
+        "test/device/2", AdapterType.BASE
+    )
+    assert isinstance(base_adapter, BaseAdapter)
+
+def test_get_or_create_subarray_adapter(tango_context_multitest):
+    factory = AdapterFactory()
+    subarray_adapter = factory.get_or_create_adapter(
+        "test/device/1", AdapterType.SUBARRAY
+    )
+    assert isinstance(subarray_adapter, SubArrayAdapter)
