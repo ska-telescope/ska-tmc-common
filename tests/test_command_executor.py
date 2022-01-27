e76db45ac2a87c4f2751123646e1ff2f064ec3e4
Aditya Dange <adityadange.ska@gmail.com>
2022-01-27 20:03:29 +0530

    AT1-1058 Fix linting

--- tests/test_command_executor.py
+++ tests/test_command_executor.py
@@ -1,9 +1,11 @@
 import pytest
-from ska_tmc_common.command_executor import CommandExecutor
-from tests.settings import logger
-from ska_tmc_common.tmc_command import TMCCommand
 
-class TestCommand():
+from ska_tmc_common.command_executor import CommandExecutor
+from ska_tmc_common.tmc_command import TMCCommand
+from tests.settings import logger
+
+
+class TestCommand(TMCCommand):
     def __init__(self, logger=logger):
         self.logger = logger
 
@@ -19,10 +21,11 @@ def test_enqueue_command():
     id = executor.enqueue_command(command)
     assert "TestCommand" in id
 
+
 @pytest.mark.executor
 def test_queue_full():
     executor = CommandExecutor(logger, 2)
     command = TestCommand()
-    id = executor.enqueue_command(command)
-    id = executor.enqueue_command(command)
-    assert executor.queue_full == True
+    executor.enqueue_command(command)
+    executor.enqueue_command(command)
+    assert executor.queue_full is True
