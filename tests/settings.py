5d85e01998e28e87a31f315223b9d134972f42cb
Kalyani Thigale <kalyanit.ska@gmail.com>
2022-02-07 12:02:22 +0530

    AT1-1061 Resolve review comments

--- tests/settings.py
+++ tests/settings.py
@@ -50,69 +50,8 @@ def create_cm(p_monitoring_loop=True, p_event_receiver=True):
         _event_receiver=p_event_receiver,
     )
 
-    # for dev in DEVICE_LIST:
-    #     cm.add_device(dev)
     start_time = time.time()
-    # num_devices = len(DEVICE_LIST)
-    # if not p_monitoring_loop:
-    #     return cm, start_time
-    # while num_devices != len(cm.checked_devices):
-    #     time.sleep(SLEEP_TIME)
-    #     elapsed_time = time.time() - start_time
-    #     if elapsed_time > TIMEOUT:
-    #         pytest.fail("Timeout occurred while executing the test")
-
     return cm, start_time
-
-
-# def create_cm_no_faulty_devices(
-#     tango_context,
-#     p_monitoring_loop,
-#     p_event_receiver
-# ):
-#     logger.info("%s", tango_context)
-
-#     cm, start_time = create_cm(
-#         p_monitoring_loop, p_event_receiver )
-#     num_faulty = count_faulty_devices(cm)
-#     assert num_faulty == 0
-#     elapsed_time = time.time() - start_time
-#     logger.info("checked %s devices in %s", num_faulty, elapsed_time)
-#     return cm
-
-
-# def ensure_telescope_state(cm, state, expected_elapsed_time):
-#     start_time = time.time()
-#     elapsed_time = 0
-#     while cm.component.telescope_state != state:
-#         elapsed_time = time.time() - start_time
-#         time.sleep(0.1)
-#         if elapsed_time > TIMEOUT:
-#             pytest.fail("Timeout occurred while executing the test")
-#     assert elapsed_time < expected_elapsed_time
-
-
-# def ensure_tmc_op_state(cm, state, expected_elapsed_time):
-#     start_time = time.time()
-#     elapsed_time = 0
-#     while cm.component.tmc_op_state != state:
-#         elapsed_time = time.time() - start_time
-#         time.sleep(0.1)
-#         if elapsed_time > TIMEOUT:
-#             pytest.fail("Timeout occurred while executing the test")
-#     assert elapsed_time < expected_elapsed_time
-
-
-# def ensure_imaging(cm, value, expected_elapsed_time):
-#     start_time = time.time()
-#     elapsed_time = 0
-#     while cm.component.imaging != value:
-#         elapsed_time = time.time() - start_time
-#         time.sleep(0.1)
-#         if elapsed_time > TIMEOUT:
-#             pytest.fail("Timeout occurred while executing the test")
-#     assert elapsed_time < expected_elapsed_time
-
 
 def set_devices_state(devices, state, devFactory, cm, expected_elapsed_time):
     for device in devices:
