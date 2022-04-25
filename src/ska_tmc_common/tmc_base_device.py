6d93eebe282d67cac89e0afea62ab84c354d82f0
shraddhabajare <shraddhab.ska@gmail.com>
2022-04-25 19:05:15 +0530

    AT1-1101 - Resolve review comments

--- src/ska_tmc_common/tmc_base_device.py
+++ src/ska_tmc_common/tmc_base_device.py
@@ -1,65 +1,37 @@
 import json
 
 from ska_tango_base import SKABaseDevice
-from tango import AttrWriteType
 from tango.server import attribute, device_property
 
 
 class TMCBaseDevice(SKABaseDevice):
     """
-    Class for common attributes and device properties within Tmc devices.
+    Class for common attributes.
     """
+
+    # -----------------
+    # Device Properties
+    # -----------------
+    SleepTime = device_property(dtype="DevFloat", default_value=1)
 
     # -----------------
     # Attributes
     # -----------------
 
-    commandInProgress = attribute(
+    @attribute(
         dtype="DevString",
-        access=AttrWriteType.READ,
-        doc="commandInProgress attribute.",
+        doc="commandInProgress attribute of TMC Nodes .",
     )
+    def commandInProgress(self):
+        if not issubclass(TMCBaseDevice, self.__class__):
+            return self.component_manager.command_executor.command_in_progress
 
-    commandExecuted = attribute(
+    @attribute(
         dtype=(("DevString",),),
         max_dim_x=4,
         max_dim_y=1000,
     )
-
-    lastDeviceInfoChanged = attribute(
-        dtype="DevString",
-        access=AttrWriteType.READ,
-        doc="Json String representing the last device information changed in the internal model.",
-    )
-
-    lastCommandExecuted = attribute(
-        dtype="DevString",
-        access=AttrWriteType.READ,
-        doc="Last command executed as string: uniqueid, command name, .result and message",
-    )
-
-    internalModel = attribute(
-        dtype="DevString",
-        access=AttrWriteType.READ,
-        doc="Json String representing the entire internal model.",
-    )
-
-    transformedInternalModel = attribute(
-        dtype="DevString",
-        access=AttrWriteType.READ,
-        doc="Json String representing the entire internal model transformed for better reading.",
-    )
-    # -----------------
-    # Device Properties
-    # -----------------
-
-    SleepTime = device_property(dtype="DevFloat", default_value=1)
-
-    def read_commandInProgress(self):
-        if not issubclass(TMCBaseDevice, self.__class__):
-            return self.component_manager.command_executor.command_in_progress
-
-    def read_commandExecuted(self):
+    def commandExecuted(self):
         """Return the commandExecuted attribute."""
         if not issubclass(TMCBaseDevice, self.__class__):
             result = []
@@ -75,10 +47,18 @@ class TMCBaseDevice(SKABaseDevice):
                 result.append(single_res)
             return result
 
-    def read_lastDeviceInfoChanged(self):
-        return self._LastDeviceInfoChanged
+    @attribute(
+        dtype="DevString",
+        doc="Json String representing the last device info changed in the internal model.",
+    )
+    def lastDeviceInfoChanged(self):
+        return self.last_device_info_changed
 
-    def read_lastCommandExecuted(self):
+    @attribute(
+        dtype="DevString",
+        doc="Last command executed as string: uniqueid, command name, .result and message",
+    )
+    def lastCommandExecuted(self):
         """Return the lastCommandExecuted attribute as list of string."""
         if not issubclass(TMCBaseDevice, self.__class__):
             for command_executed in reversed(
@@ -92,7 +72,12 @@ class TMCBaseDevice(SKABaseDevice):
                 )
                 return single_res
 
-    def read_transformedInternalModel(self):
+    @attribute(
+        dtype="DevString",
+        doc="Json String representing the entire internal model transformed for better reading.",
+    )
+    def transformedInternalModel(self):
+
         if not issubclass(TMCBaseDevice, self.__class__):
             json_model = json.loads(self.component_manager.component.to_json())
             result = {}
@@ -101,23 +86,25 @@ class TMCBaseDevice(SKABaseDevice):
                 del dev["dev_name"]
                 result[dev_name] = dev
             if "CentralNode" in str(self.__class__):
-                """Executes CentralNode device's read method."""
-                result = self.read_device_transformedInternalModel(
+                result = self.read_CN_transformedInternalModel(
                     result, json_model
                 )
                 return json.dumps(result)
             elif "SubarrayNode" in str(self.__class__):
-                """Executes SubarrayNode device's read method."""
-                result = self.read_device_transformedInternalModel(
+                result = self.read_SN_transformedInternalModel(
                     result, json_model
                 )
                 return json.dumps(result)
 
-    def read_internalModel(self):
+    @attribute(
+        dtype="DevString",
+        doc="Json String representing the entire internal model.",
+    )
+    def internalModel(self):
         if not issubclass(TMCBaseDevice, self.__class__):
             internal_model = self.component_manager.component.to_json()
             if "SubarrayNode" in str(self.__class__):
-                sn_internal_model = self.read_device_internalModel(
+                sn_internal_model = self.read_SN_internalModel(
                     json.loads(internal_model)
                 )
                 return json.dumps(sn_internal_model)
