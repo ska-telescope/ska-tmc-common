53002efbab26c3061f2880b55a8e73f9ef74d26e
user <jayant.ska@gmail.com>
2022-05-02 15:02:57 +0530

    AT1-1117 - Resolve review comments

--- src/ska_tmc_common/tmc_base_device.py
+++ src/ska_tmc_common/tmc_base_device.py
@@ -75,24 +75,76 @@ class TMCBaseDevice(SKABaseDevice):
         doc="Json String representing the entire internal model transformed for better reading.",
     )
     def transformedInternalModel(self):
-        return self.read_device_transformedInternalModel()
+        return self.transformedInternalModel_read()
 
-    def read_device_transformedInternalModel(self):
+    def transformedInternalModel_read(self):
+        """
+        This method consists of basic  read implementation of transformedInternalModel and
+        must be overloaded to add additional values to result dictionary.
+        Returns json string with device data.
+    
+        Sample Output:
+            {'mccs':
+                {
+                'state': 'DevState.UNKNOWN',
+                'healthState': 'HealthState.UNKNOWN',
+                'ping': '-1',
+                'last_event_arrived': 'None',
+                'unresponsive': 'False',
+                'exception': 'None',
+                'id': -1,
+                'pointingState': 'PointingState.NONE'
+                }
+            }
+
+        """
         json_model = json.loads(self.component_manager.component.to_json())
         result = {}
-        for dev in json_model["devices"]:
+        if (
+            "TmcLeafNodeComponentManager"
+            not in self.component_manager.__class__.__bases__
+        ):
+            for dev in json_model["devices"]:
+                dev_name = dev["dev_name"]
+                del dev["dev_name"]
+                result[dev_name] = dev
+        else:
+            dev = json_model["device"]
             dev_name = dev["dev_name"]
             del dev["dev_name"]
             result[dev_name] = dev
-
-        return result, json_model
+        return json.dumps(result)
 
     @attribute(
         dtype="DevString",
         doc="Json String representing the entire internal model.",
     )
     def internalModel(self):
-        return self.read_device_internalModel()
+        return self.internalModel_read()
 
-    def read_device_internalModel(self):
+    def internalModel_read(self):
+        """
+        This method consists of basic  read implementation of InternalModel and
+        must be overloaded to add additional attribute values to internal model.
+        Returns json string representing internal model with basic attributes only.
+
+    Sample Output:
+        {
+            "subarray_health_state": "HealthState.UNKNOWN",
+            "devices":
+            [
+                {
+                "dev_name": "mccs",
+                "state": "DevState.UNKNOWN",
+                "healthState": "HealthState.UNKNOWN",
+                 "ping": "-1",
+                 "last_event_arrived": "None",
+                 "unresponsive": "False",
+                 "exception": "None",
+                  "id": -1,
+                  "pointingState": "PointingState.NONE"
+                  }
+            ]
+        }
+        """
         return self.component_manager.component.to_json()
