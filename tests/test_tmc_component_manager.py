0d0c060b4e61085d2ecb759ea9ac23362a8eaa1e
shraddhabajare <shraddhab.ska@gmail.com>
2022-07-04 11:09:59 +0530

    SAH-1142 - Add unit test cases for ComponentManager

--- tests/test_tmc_component_manager.py
+++ tests/test_tmc_component_manager.py
@@ -1,9 +1,8 @@
 import logging
-
+import pytest
 from ska_tmc_common.op_state_model import TMCOpStateModel
 from ska_tmc_common.test_helpers.helper_tmc_device import DummyComponent
 from ska_tmc_common.tmc_component_manager import TmcComponentManager
-
 logger = logging.getLogger(__name__)
 
 
@@ -39,3 +38,43 @@ def test_update_device():
     cm.update_device_info(device_info)
     new_device_info = cm.get_device("dummy/monitored/device")
     assert new_device_info.unresponsive is True
+
+def test_telescope_on():
+    op_state_model = TMCOpStateModel(logger)
+    dummy_component = DummyComponent(logger)
+    cm = TmcComponentManager(op_state_model, dummy_component, logger)
+    # raise NotImplementedError 
+    with pytest.raises(NotImplementedError):
+        cm.telescope_on()
+
+def test_telescope_off():
+    op_state_model = TMCOpStateModel(logger)
+    dummy_component = DummyComponent(logger)
+    cm = TmcComponentManager(op_state_model, dummy_component, logger)
+    # raise NotImplementedError 
+    with pytest.raises(NotImplementedError):
+        cm.telescope_off()
+
+def test_telescope_standby():
+    op_state_model = TMCOpStateModel(logger)
+    dummy_component = DummyComponent(logger)
+    cm = TmcComponentManager(op_state_model, dummy_component, logger)
+    # raise NotImplementedError 
+    with pytest.raises(NotImplementedError):
+        cm.telescope_standby()
+
+def test_assign_resources():
+    op_state_model = TMCOpStateModel(logger)
+    dummy_component = DummyComponent(logger)
+    cm = TmcComponentManager(op_state_model, dummy_component, logger)
+    # raise NotImplementedError 
+    with pytest.raises(NotImplementedError):
+        cm.assign_resources()
+
+def test_release_resources():
+    op_state_model = TMCOpStateModel(logger)
+    dummy_component = DummyComponent(logger)
+    cm = TmcComponentManager(op_state_model, dummy_component, logger)
+    # raise NotImplementedError 
+    with pytest.raises(NotImplementedError):
+        cm.release_resources()
