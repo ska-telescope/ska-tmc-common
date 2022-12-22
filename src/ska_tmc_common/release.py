3433acab9280ad0eb8ad029d91164b83c8897981
Bhushan Parakh <bhushan.ska@gmail.com>
2022-12-22 18:48:50 +0530

    HM-110: Version update

--- src/ska_tmc_common/release.py
+++ src/ska_tmc_common/release.py
@@ -7,7 +7,7 @@
 """Release information for ska-tmc-common Python Package"""
 
 name = """ska-tmc-common"""
-version = "0.1.51"
+version = "0.1.52"
 version_info = version.split(".")
 description = """A set of common classes for TMC devices."""
 author = "SKA India NCRA"
