78e33844e003ebc27a7f5ad72f65ecadf1280d92
Roshan Tayde <roshant.ska@gmail.com>
2023-03-17 18:13:10 +0530

    HM-169: Resolve review comments.

--- src/ska_tmc_common/enum.py
+++ src/ska_tmc_common/enum.py
@@ -13,6 +13,9 @@ class PointingState(IntEnum):
 
 @unique
 class DishMode(IntEnum):
+    #ska-mid-dish-manager is having dependency conflicts with ska-tmc-common
+    #So redefined DishMode enum, which reflects the ska-mid-dish-manager DishMode enum.
+    #We will work out on this separately once dish manager is sorted.  
     STARTUP = 0
     SHUTDOWN = 1
     STANDBY_LP = 2
