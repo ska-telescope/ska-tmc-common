551ec81c39a395b0accc9cc1cf302a2ddc9ea7b3
Bhushan Parakh <bhushan.ska@gmail.com>
2023-03-31 18:07:03 +0530

    HM-192

--- src/ska_tmc_common/tmc_command.py
+++ src/ska_tmc_common/tmc_command.py
@@ -1,19 +1,22 @@
-import logging
+import threading
 import time
+from logging import Logger
 from operator import methodcaller
-from typing import Optional, Tuple
+from typing import Callable, Tuple
 
 from ska_tango_base.commands import ResultCode
-from tango import ConnectionFailed, DevFailed
+from tango import ConnectionFailed, DevFailed, EnsureOmniThread
 
 from ska_tmc_common.adapters import AdapterFactory, AdapterType
+from ska_tmc_common.enum import TimeoutState
+from ska_tmc_common.timeout_callback import TimeoutCallback
 
 
 class BaseTMCCommand:
     def __init__(
         self,
         component_manager,
-        logger: Optional[logging.Logger] = None,
+        logger: Logger,
         *args,
         **kwargs,
     ):
@@ -59,19 +62,99 @@ class BaseTMCCommand:
                     e,
                 )
 
-    def generate_command_result(self, result_code, message):
-        if result_code == ResultCode.FAILED:
-            self.logger.error(message)
+    def generate_command_result(
+        self, result_code: ResultCode, message: str
+    ) -> Tuple[ResultCode, str]:
         self.logger.info(message)
-        return (result_code, message)
+        return result_code, message
 
-    def adapter_error_message_result(self, dev_name, e):
+    def adapter_error_message_result(
+        self, dev_name: str, e: str
+    ) -> Tuple[ResultCode, str]:
         message = f"Error in creating adapter for {dev_name}: {e}"
         self.logger.error(message)
         return ResultCode.FAILED, message
 
-    def do(self, argin=None):
-        raise NotImplementedError("This method must be inherited!")
+    def do(self, argin=None) -> NotImplementedError:
+        raise NotImplementedError(
+            "This method must be implemented by command class"
+        )
+
+    def update_task_status(self, result: ResultCode) -> NotImplementedError:
+        raise NotImplementedError(
+            "This method must be implemented by command class"
+        )
+
+    def start_tracker_thread(
+        self,
+        state_function: Callable,
+        expected_state,
+        id: str,
+        timeout_callback: TimeoutCallback,
+    ) -> None:
+        """Creates and starts a thread that will keep track of the State/
+        obsState change to be monitored (For confirming command completion.
+        Currently only supports obsState change.) and the timeout callback to
+        montior the timeout.
+
+        :param expected_state: Expected state of the device in case of
+                    successful command execution.
+
+        :param id: Id for TimeoutCallback class object.
+
+        :param timeout_callback: An instance of TimeoutCallback class that acts
+                    as a callable functions to call in the event of timeout.
+        """
+        self.tracker_thread = threading.Thread(
+            target=self.track_timeout_and_transition,
+            args=[state_function, expected_state, id, timeout_callback],
+        )
+        self._stop = False
+        self.logger.info("Starting tracker thread")
+        self.tracker_thread.start()
+
+    def track_timeout_and_transition(
+        self,
+        state_function: Callable,
+        expected_state,
+        id: str,
+        timeout_callback: TimeoutCallback,
+    ) -> None:
+        """Keeps track of the obsState change and the timeout callback to
+        determine whether timeout has occured or the command completed
+        successfully. Logs the result for now.
+
+        :param expected_state: Expected state of the device in case of
+                    successful command execution.
+
+        :param id: Id for TimeoutCallback class object.
+
+        :param timeout_callback: An instance of TimeoutCallback class that acts
+                    as a callable functions to call in the event of timeout.
+        """
+        with EnsureOmniThread():
+            while not self._stop:
+                if state_function() == expected_state:
+                    self.logger.info(
+                        "State change has occured, command succeded"
+                    )
+                    self.update_task_status(result=ResultCode.OK)
+                    self.stop_tracker_thread()
+                elif timeout_callback.assert_against_call(
+                    id, TimeoutState.OCCURED
+                ):
+                    self.logger.error("Timeout has occured, command failed")
+                    self.update_task_status(result=ResultCode.FAILED)
+                    self.stop_tracker_thread()
+                time.sleep(0.1)
+
+    def stop_tracker_thread(self) -> None:
+        """External stop method for stopping the timer thread as well as the
+        tracker thread."""
+        if self.tracker_thread.is_alive():
+            self.logger.info("Stopping tracker thread")
+            self._stop = True
+            self.component_manager.stop_timer()
 
 
 class TMCCommand(BaseTMCCommand):
