31f39f4648a63112a4d9ec09253c0d70089ce95f
Roshan Tayde <roshant.ska@gmail.com>
2023-04-06 18:04:56 +0530

    HM-188: Add AbortCommands in DishAdapter Class

--- src/ska_tmc_common/release.py
+++ src/ska_tmc_common/release.py
@@ -7,7 +7,7 @@
 """Release information for ska-tmc-common Python Package"""
 
 name = """ska-tmc-common"""
-version = "0.3.0"
+version = "0.3.1"
 version_info = version.split(".")
 description = """A set of common classes for TMC devices."""
 author = "Team Himalaya, Team Sahyadri"
