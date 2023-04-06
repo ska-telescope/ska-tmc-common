8aa7167cff5848e39842c2e3a0e892af7c1bf47f
Bhushan Parakh <bhushan.ska@gmail.com>
2023-04-06 12:03:39 +0530

    HM-195: Version update

--- src/ska_tmc_common/release.py
+++ src/ska_tmc_common/release.py
@@ -7,7 +7,7 @@
 """Release information for ska-tmc-common Python Package"""
 
 name = """ska-tmc-common"""
-version = "0.2.2"
+version = "0.2.3"
 version_info = version.split(".")
 description = """A set of common classes for TMC devices."""
 author = "Team Himalaya, Team Sahyadri"
