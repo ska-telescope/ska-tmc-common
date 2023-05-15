0374745304a7627f0cb3062a922778f6e6b4e28d
Bhushan Parakh <bhushan.ska@gmail.com>
2023-05-15 12:23:19 +0530

    HM-223: Resolving comments

--- src/ska_tmc_common/lrcr_callback.py
+++ src/ska_tmc_common/lrcr_callback.py
@@ -16,7 +16,7 @@ class LRCRCallback:
     def __call__(
         self,
         command_id: str,
-        exception_state: ResultCode,
+        result_code: ResultCode,
         exception_msg: str = "",
         **kwargs: Any
     ) -> Optional[ValueError]:
@@ -25,33 +25,33 @@ class LRCRCallback:
 
         :param command_id: ID of command.
 
-        :param exception_state: Enum of ExceptionState class.
+        :param result_code: Enum of ResultCode class.
 
         :param exception_msg: String of execption message. (Optional)
 
         :return: Raises ValueError
         """
         if command_id in self.command_data:
-            self.command_data[command_id]["exception_state"] = exception_state
+            self.command_data[command_id]["result_code"] = result_code
             self.command_data[command_id]["exception_message"] = exception_msg
             for key, value in kwargs.items():
                 self.command_data[command_id][key] = value
         else:
             self.command_data[command_id] = {
-                "exception_state": exception_state,
+                "result_code": result_code,
                 "exception_message": exception_msg,
             }
             for key, value in kwargs.items():
                 self.command_data[command_id][key] = value
 
     def assert_against_call(
-        self, command_id: str, exception_state: ResultCode, **kwargs: Any
+        self, command_id: str, result_code: ResultCode, **kwargs: Any
     ) -> bool:
-        """Assertion method to check if the desired exception state change has occured."""
+        """Assertion method to check if the desired result code change has occured."""
         if command_id not in self.command_data:
             return False
 
-        if exception_state != self.command_data[command_id]["exception_state"]:
+        if result_code != self.command_data[command_id]["result_code"]:
             return False
 
         try:
