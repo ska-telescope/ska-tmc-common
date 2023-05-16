91aef3ddd1016e0976805b6678de1381a13b943b
muskansharma_ <muskans.ska@gmail.com>
2023-05-16 10:24:15 +0530

    HM-229:added some test case for subarray commands

--- tests/unit/test_helper_subarray_device.py
+++ tests/unit/test_helper_subarray_device.py
@@ -1,10 +1,22 @@
 import time
 
+import pytest
 from ska_control_model import ObsState
 from ska_tango_base.commands import ResultCode
 
 from ska_tmc_common import DevFactory
 from tests.settings import SUBARRAY_DEVICE
+
+commands_with_argin = ["AssignResources", "Restart", "Scan"]
+commands_without_argin = [
+    "On",
+    "Off",
+    "ReleaseAllResources",
+    "EndScan",
+    "ObsReset",
+    "Restart",
+    "Standby",
+]
 
 
 def test_set_delay(tango_context):
@@ -23,17 +35,6 @@ def test_set_defective(tango_context):
     assert subarray_device.defective
 
 
-def test_assign_resources(tango_context):
-    dev_factory = DevFactory()
-    subarray_device = dev_factory.get_device(SUBARRAY_DEVICE)
-    result, message = subarray_device.AssignResources("")
-    assert result[0] == ResultCode.OK
-    assert message[0] == ""
-    assert subarray_device.obsstate == ObsState.RESOURCING
-    time.sleep(2.5)
-    assert subarray_device.obsstate == ObsState.IDLE
-
-
 def test_assign_resources_defective(tango_context):
     dev_factory = DevFactory()
     subarray_device = dev_factory.get_device(SUBARRAY_DEVICE)
@@ -44,3 +45,25 @@ def test_assign_resources_defective(tango_context):
         message[0] == "Device is Defective, cannot process command completely."
     )
     assert subarray_device.obsstate == ObsState.RESOURCING
+
+
+@pytest.mark.dd
+@pytest.mark.parametrize("command", commands_with_argin)
+def test_command_with_argin(tango_context, command):
+    dev_factory = DevFactory()
+    subarray_device = dev_factory.get_device(SUBARRAY_DEVICE)
+    result, message = getattr(subarray_device, command)("")
+    assert result[0] == ResultCode.OK
+    assert message[0] == ""
+    assert subarray_device.obsstate == ObsState.RESOURCING
+    time.sleep(2.5)
+    assert subarray_device.obsstate == ObsState.IDLE
+
+
+@pytest.mark.dd
+@pytest.mark.parametrize("command", commands_without_argin)
+def test_command_without_argin(tango_context, command):
+    dev_factory = DevFactory()
+    subarray_device = dev_factory.get_device(SUBARRAY_DEVICE)
+    result, message = getattr(subarray_device, command)()
+    assert result[0] == ResultCode.OK
