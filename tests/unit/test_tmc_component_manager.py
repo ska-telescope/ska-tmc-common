8ccbd1112de859c2e2e94b43e74855eecac7ae0b
muskansharma_ <muskans.ska@gmail.com>
2023-05-17 16:15:21 +0530

    HM-229:Removed mark

--- tests/unit/test_tmc_component_manager.py
+++ tests/unit/test_tmc_component_manager.py
@@ -106,7 +106,6 @@ def test_start_liveliness_probe_single_device(component_manager):
     assert component_manager.liveliness_probe_object is not None
 
 
-@pytest.mark.dd
 def test_device_failed(component_manager):
     # Test if device_failed sets the device's exception and does not raise an exception
     exception = "test exception"
@@ -121,7 +120,6 @@ def test_update_device_info(component_manager):
     assert component_manager.get_device() == device_info
 
 
-@pytest.mark.dd
 def test_update_ping_info(component_manager):
     # Test if update_ping_info sets the device's ping and does not raise an exception
     ping = 123
