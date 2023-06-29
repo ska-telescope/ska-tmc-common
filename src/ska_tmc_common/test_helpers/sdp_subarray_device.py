989e635bdc72d662b6508020ab01ded1ab45157b
JayantKumbhar <jayant.ska@gmail.com>
2023-06-29 18:36:54 +0530

    SAH-1350: Enable commented code

--- src/ska_tmc_common/test_helpers/sdp_subarray_device.py
+++ src/ska_tmc_common/test_helpers/sdp_subarray_device.py
@@ -175,7 +175,7 @@ class SdpSubarrayDevice(Device):
         if self._defective:
             self.raise_exception_for_defective_device()
 
-        # self.logger.info(f"Argin on SdpSubarray helper: {argin}")
+        self.logger.info(f"Argin on SdpSubarray helper: {argin}")
         input = json.loads(argin)
 
         if "eb_id" not in input["execution_block"]:
