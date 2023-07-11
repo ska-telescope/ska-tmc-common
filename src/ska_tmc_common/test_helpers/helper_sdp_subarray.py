f5be9c16a5be00515c95304f56aee57ce922db38
roshant.ska <roshant.ska@gmail.com>
2023-07-11 23:38:53 +0530

    HM-252: Minor changes.

--- src/ska_tmc_common/test_helpers/helper_sdp_subarray.py
+++ src/ska_tmc_common/test_helpers/helper_sdp_subarray.py
@@ -157,6 +157,8 @@ class HelperSdpSubarray(HelperSubArrayDevice):
             return
 
         if self._raise_exception:
+            self._obs_state = ObsState.RESOURCING
+            self.push_change_event("obsState", self._obs_state)
             self.raise_exception_for_defective_device(
                 command_name="SdpSubarray.ReleaseAllResources",
                 exception=f"Exception occurred on device: {self.get_name()}"
