c33271f518cf2c52a0246d38a85bbb8f531e14d7
shraddhabajare <shraddhab.ska@gmail.com>
2023-07-17 15:04:13 +0530

    SAH-1363 - Fix unit test case

--- tests/unit/test_helper_subarray_device.py
+++ tests/unit/test_helper_subarray_device.py
@@ -48,10 +48,9 @@ def test_clear_commandCallInfo(tango_context):
     dev_factory = DevFactory()
     subarray_device = dev_factory.get_device(SUBARRAY_DEVICE)
     _, _ = subarray_device.command_inout("Configure", "")
-    # assert command_call_info[0] == ("Configure", "")
     subarray_device.command_inout("ClearCommandCallInfo")
     command_call_info = subarray_device.commandCallInfo
-    assert command_call_info == []
+    assert command_call_info == None
 
 
 def test_set_defective(tango_context):
