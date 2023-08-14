0f370798a9605514bcdc70c3d826cf85005b100a
muskansharma_ <muskans.ska@gmail.com>
2023-08-14 16:15:15 +0530

    HM-296:resolving errors

--- tests/unit/test_helper_csp.py
+++ tests/unit/test_helper_csp.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 from ska_tango_base.commands import ResultCode
 
@@ -20,7 +22,7 @@ def test_csp_commands(tango_context, command):
 def test_csp_command_defective(tango_context, command):
     dev_factory = DevFactory()
     csp_device = dev_factory.get_device(CSP_DEVICE)
-    csp_device.SetDefective(True)
+    csp_device.SetDefective(json.dumps({"enabled": True}))
     result, message = csp_device.command_inout(command, "")
     assert result[0] == ResultCode.FAILED
-    assert message[0] == "Device is defective, cannot process command."
+    assert message[0] == "Default exception."
