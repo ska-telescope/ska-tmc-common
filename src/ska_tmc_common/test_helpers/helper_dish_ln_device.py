75c58bdc5a00c95887c5310310f876784feab92d
Bhushan Parakh <bhushan.ska@gmail.com>
2023-11-09 19:13:40 +0530

    HM-367: Setting change event for LRCR attribute

--- src/ska_tmc_common/test_helpers/helper_dish_ln_device.py
+++ src/ska_tmc_common/test_helpers/helper_dish_ln_device.py
@@ -65,6 +65,9 @@ class HelperDishLNDevice(HelperBaseDevice):
             super().do()
             self._device.set_change_event("commandCallInfo", True, False)
             self._device.set_change_event("isSubsystemAvailable", True, False)
+            self._device.set_change_event(
+                "longRunningCommandResult", True, False
+            )
             self._device.set_change_event("actualPointing", True, False)
             return (ResultCode.OK, "")
 
