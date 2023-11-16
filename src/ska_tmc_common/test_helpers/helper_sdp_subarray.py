aa1aeb46673d6265665a01901c7612cddea28040
Mansi Shirore <mansis.ska@gmail.com>
2023-11-16 18:43:20 +0530

    SAH-1432: Updated sdp helper device

--- src/ska_tmc_common/test_helpers/helper_sdp_subarray.py
+++ src/ska_tmc_common/test_helpers/helper_sdp_subarray.py
@@ -56,11 +56,19 @@ class HelperSdpSubarray(HelperSubArrayDevice):
         # pylint:disable=line-too-long
         self._receive_addresses = (
             '{"science_A":{"host":[[0,"192.168.0.1"],[2000,"192.168.0.1"]],"port":['
-            '[0,9000,1],[2000,9000,1]]},"target:a":{"vis0":{"function":"visibilities"'
-            ',"host":[[0,"proc-pb-test-20220916-00000-test-receive-0.receive.test-sdp"]]'
-            ',"port":[[0,9000,1]],"pointing_cal":"test-sdp/queueconnector/01"}},'
-            '"calibration:b":{"vis0":{"function":"visibilities","host":'
-            '[[0,"proc-pb-test-20220916-00000-test-receive-0.receive.test-sdp"]],'
+            '[0,9000,1],[2000,9000,1]]},"target:a":{"vis0":{'
+            '"function":"visibilities","host":[[0,'
+            '"proc-pb-test-20220916-00000-test-receive-0.receive.test-sdp"]],'
+            '"port":[[0,9000,1]]}},"calibration:b":{"vis0":{'
+            '"function":"visibilities","host":[[0,'
+            '"proc-pb-test-20220916-00000-test-receive-0.receive.test-sdp"]],'
+            '"port":[[0,9000,1]]}},"science_B":{"host":[[0,"192.168.0.1"],[2000,"192.168.0.1"]],"port":['
+            '[0,9000,1],[2000,9000,1]]},"target:a":{"vis0":{'
+            '"function":"visibilities","host":[[0,'
+            '"proc-pb-test-20220916-00000-test-receive-0.receive.test-sdp"]],'
+            '"port":[[0,9000,1]]}},"calibration:b":{"vis0":{'
+            '"function":"visibilities","host":[[0,'
+            '"proc-pb-test-20220916-00000-test-receive-0.receive.test-sdp"]],'
             '"port":[[0,9000,1]]}}}'
         )
 
