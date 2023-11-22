3c2f0d25e95adb96315b46df111b83bdd71b5939
Muskan Sharma <muskans.ska@gmail.com>
2023-11-22 15:49:10 +0530

    HM-352: Fixed small bug in common.

--- src/ska_tmc_common/test_helpers/helper_subarray_leaf_device.py
+++ src/ska_tmc_common/test_helpers/helper_subarray_leaf_device.py
@@ -215,6 +215,7 @@ class HelperSubarrayLeafDevice(HelperBaseDevice):
         :type: str
         """
         self.logger.info("Setting device obsState to %s", value)
+        self.push_change_event("obsState", self._obs_state)
         self._obs_state = value
 
     @command(
