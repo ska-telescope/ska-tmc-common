f0d1f5e1757ce976040f57db9bad33e5f89fea0b
Roshan Tayde <roshant.ska@gmail.com>
2024-11-17 08:03:32 +0000

    HM-567: Remove default green mode from dev factory

--- src/ska_tmc_common/dev_factory.py
+++ src/ska_tmc_common/dev_factory.py
@@ -33,24 +33,19 @@ class DevFactory:
         self.logger = logging.getLogger(__name__)
         self.default_green_mode = green_mode
 
-    def get_device(self, dev_name: str, green_mode=None) -> tango.DeviceProxy:
+    def get_device(self, dev_name: str) -> tango.DeviceProxy:
         """
         Create (if not done before) a DeviceProxy for the Device fqnm
 
         :param dev_name: Device name
-        :param green_mode: tango.GreenMode (synchronous by default)
 
         :return: DeviceProxy
         """
-        if green_mode is None:
-            green_mode = self.default_green_mode
         # import debugpy; debugpy.debug_this_thread()
         if DevFactory._test_context is None:
             if dev_name not in self.dev_proxys:
                 self.logger.debug("Creating Proxy for %s", dev_name)
-                self.dev_proxys[dev_name] = tango.DeviceProxy(
-                    dev_name, green_mode=green_mode
-                )
+                self.dev_proxys[dev_name] = tango.DeviceProxy(dev_name)
             return self.dev_proxys[dev_name]
 
         return DevFactory._test_context.get_device(dev_name)
