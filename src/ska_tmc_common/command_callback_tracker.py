8d36bb85317764e15bf4edb0642c45f4791c3a51
yogeshk.ska <yogeshk.ska@gmail.com>
2024-12-23 16:08:41 +0530

    hm-580: clean up called

--- src/ska_tmc_common/command_callback_tracker.py
+++ src/ska_tmc_common/command_callback_tracker.py
@@ -108,6 +108,11 @@ class CommandCallbackTracker:
         try:
             if not self.command_completed and not self.abort_event.is_set():
                 if self.command_id in self.lrcr_callback.command_data:
+                    self.logger.info(
+                        "Command id %s found in %s",
+                        self.command_id,
+                        self.lrcr_callback.command_data,
+                    )
                     self.command_class_instance.update_task_status(
                         result=(
                             ResultCode.FAILED,
