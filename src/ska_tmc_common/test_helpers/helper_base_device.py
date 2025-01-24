6353f7d0c40af02b43f84b4fdbdf9dcc4389c26f
Akshay Sonawane <akshayss.ska@gmail.com>
2025-01-24 11:30:12 +0530

    HM-605: update the test case

--- src/ska_tmc_common/test_helpers/helper_base_device.py
+++ src/ska_tmc_common/test_helpers/helper_base_device.py
@@ -547,7 +547,7 @@ class HelperBaseDevice(SKABaseDevice):
         return True
 
     @command(
-        dtype_in=AdminMode,
+        dtype_in=int,
         doc_in="The input string in JSON format.",
         dtype_out="DevVarLongStringArray",
         doc_out="(ReturnType, 'informational message')",
