4100fc5465dd6f3e3d0a776150a34c05114c4738
truptin.ska <truptin.ska@gmail.com>
2025-03-10 19:33:45 +0530

    SAH-1670:Event Receiver Updated

--- src/ska_tmc_common/v1/event_receiver.py
+++ src/ska_tmc_common/v1/event_receiver.py
@@ -9,7 +9,6 @@ from time import sleep
 from typing import Callable, Optional
 
 import tango
-from ska_control_model import AdminMode
 
 from ska_tmc_common.dev_factory import DevFactory
 from ska_tmc_common.device_info import DeviceInfo
@@ -186,45 +185,24 @@ class EventReceiver:
             event.device.dev_name(), new_value
         )
 
-    def handle_obs_state_event(self, event: tango.EventData) -> None:
-        """
-        It handles the observation state events of different devices
-        """
-        if event.err:
-            error = event.errors[0]
-            self._logger.error("%s\n %s", error.reason, error.desc)
-            self._component_manager.update_event_failure(
-                event.device.dev_name()
-            )
-            return
+    def handle_obs_state_event(
+        self, event: tango.EventType.CHANGE_EVENT
+    ) -> None:
+        """Handle the Obs-State event."""
 
-        new_value = event.attr_value.value
-        self._component_manager.update_device_obs_state(
-            event.device.dev_name(), new_value
+        self._component_manager.event_queues["obsState"].put(event)
+
+    def handle_command_result_event(
+        self, event: tango.EventType.CHANGE_EVENT
+    ) -> None:
+        """Handle the LRCR event."""
+        self._component_manager.event_queues["longRunningCommandResult"].put(
+            event
         )
 
     def handle_admin_mode_event(
         self, event: tango.EventType.CHANGE_EVENT
     ) -> None:
         """Handle admin Mode change event"""
-        if self._component_manager.is_admin_mode_enabled:
-            if event.err:
-                error = event.errors[0]
-                error_msg = f"{error.reason},{error.desc}"
-                self._logger.error(error_msg)
-                self._component_manager.update_event_failure(
-                    event.device.dev_name()
-                )
-                return
-            new_value = event.attr_value.value
-            self._logger.info(
-                "Received an adminMode event with : %s for device: %s",
-                new_value,
-                event.device.dev_name(),
-            )
-            self._component_manager.update_device_admin_mode(
-                event.device.dev_name(), new_value
-            )
-            self._logger.debug(
-                "Admin Mode updated to :%s", AdminMode(new_value).name
-            )
+
+        self._component_manager.event_queues["adminMode"].put(event)
